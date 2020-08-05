2a938f5813d8ea6436cb73c0e21a31c018775620
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-05 09:57:22 +0300

    Initial Commit

---
+++ CreateSnapchatColorPicker/CreateSnapchatColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateSnapchatColorPickerApp.swift
+//  CreateSnapchatColorPicker
+//
+//  Created by ramil on 05.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct CreateSnapchatColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
